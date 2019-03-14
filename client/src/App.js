41c15fcfa3839832993a080ee1a73a7a16ef37cb
iurisajaia <sajaiaiuri@gmail.com>
2019-03-14 22:55:36 +0400

    react login / save token LS

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,21 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./components/user/login";
 import Registration from "./components/user/registration";
 import Home from "./components/home";
+import jwt_decode from "jwt-decode";
 
 class App extends Component {
+  state = {
+    active: false
+  };
+  componentDidMount() {
+    const token = localStorage.getItem("token");
+    if (token) {
+      var decoded = jwt_decode(token);
+      this.setState({ decoded, active: true });
+    }
+  }
   render() {
+    console.log(this.state.decoded);
     return (
       <BrowserRouter>
         <div className="App">
--- client/src/components/user/login.jsx
+++ client/src/components/user/login.jsx
@@ -2,23 +2,51 @@ import React, { Component } from "react";
 
 class Login extends Component {
   state = {};
+  hanldeFormSubmit = event => {
+    event.preventDefault();
+    const data = {
+      email: event.target.email.value,
+      password: event.target.password.value
+    };
+
+    // console.log(data);
+
+    fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(token => {
+        console.log(token);
+        localStorage.setItem("token", token.token);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
   render() {
     return (
-      <form className="login-form">
+      <form className="login-form" onSubmit={this.hanldeFormSubmit}>
         <div className="form-group ">
-          <label for="exampleInputEmail1">Email address</label>
+          <label htmlFor="email">Email address</label>
           <input
             type="email"
+            id="email"
             className="form-control"
             placeholder="Enter email"
           />
         </div>
         <div className="form-group">
-          <label for="exampleInputPassword1">Password</label>
+          <label htmlFor="password">Password</label>
           <input
             type="password"
             className="form-control"
             placeholder="Password"
+            id="password"
           />
         </div>
         <button type="submit" className="btn btn-block btn-primary">
