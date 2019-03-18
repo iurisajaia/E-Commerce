579b650d5be3e8a411e18e0dedfe7f57dfcdda5e
Guram Chechelashvili <guram.chechelashvili09b@geolab.edu.ge>
2019-03-19 02:47:07 +0400

    Added delete to car

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -12,12 +12,25 @@ export default class Cart extends Component {
     }
   }
 
+  remove = (id) => {
+    let items = JSON.parse(localStorage.getItem("cart"));
+    let tartetProduct = items.filter(product => {
+      return product._id == id
+    });
+    items.splice(items.indexOf(tartetProduct),1)
+    console.log(items)
+    this.setState({ cartItems:items });
+    localStorage.setItem('cart',JSON.stringify(items))
+  }
+
   render() {
     const res = this.state.cartItems ? (
       this.state.cartItems.map(el => {
         return (
           <li key={el._id} className="list-group-item">
-            <p>{el.title}</p>
+            <p>{el.title}
+              <button onClick={() => this.remove(el._id)}>Remove</button>
+            </p>
             {/* <p>{el.description}</p>
             <p>
               <img src={el.imageUrl} alt={el.imageUrl} />
